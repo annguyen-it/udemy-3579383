cfcbae9ad23edda34ea1c151078898707aa579ae
annguyen <annguyen.it.utc@gmail.com>
2022-08-27 16:23:48 +0700

    8.73: Writing tests for our main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,13 +20,30 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", port))
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() error {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
 	// change this to true when in production
 	app.InProduction = false
 
-	// setup the session
+	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -38,6 +55,7 @@ func main() {
 	templateCache, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = templateCache
@@ -47,13 +65,5 @@ func main() {
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", port))
-
-	srv := &http.Server{
-		Addr:    port,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return nil
 }
---
+++ cmd/web/routes_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"learn-golang/internal/config"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+	var app config.AppConfig
+
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+	default:
+		t.Error(fmt.Sprintf("type is not *chi.Mux, type is %T", v))
+	}
+}
