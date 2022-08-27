1f85a579a6145fbfde03b57d0ec86cb5605d38d2
annguyen <annguyen.it.utc@gmail.com>
2022-08-27 21:38:01 +0700

    8.75: Writing tests for our POST handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -67,6 +68,38 @@ var theTests = []struct {
 		params:             []postData{},
 		expectedStatusCode: http.StatusOK,
 	},
+	{
+		name:   "post-search-availability",
+		url:    "/search-availability",
+		method: "POST",
+		params: []postData{
+			{key: "start", value: "2020-01-01"},
+			{key: "end", value: "2020-01-01"},
+		},
+		expectedStatusCode: http.StatusOK,
+	},
+	{
+		name:   "post-search-availability-json",
+		url:    "/search-availability-json",
+		method: "POST",
+		params: []postData{
+			{key: "start", value: "2020-01-01"},
+			{key: "end", value: "2020-01-01"},
+		},
+		expectedStatusCode: http.StatusOK,
+	},
+	{
+		name:   "post-make-reservation",
+		url:    "/make-reservation",
+		method: "POST",
+		params: []postData{
+			{key: "first_name", value: "John"},
+			{key: "last_name", value: "Smith"},
+			{key: "email", value: "john.smith@gmail.com"},
+			{key: "phone", value: "555-444-666"},
+		},
+		expectedStatusCode: http.StatusOK,
+	},
 }
 
 func TestHandlers(t *testing.T) {
@@ -75,18 +108,28 @@ func TestHandlers(t *testing.T) {
 	defer ts.Close()
 
 	for _, e := range theTests {
+		testUrl := ts.URL + e.url
+
+		var err error
+		var resp *http.Response
+
 		if e.method == "GET" {
-			resp, err := ts.Client().Get(ts.URL + e.url)
-			if err != nil {
-				t.Log(err)
-				t.Fatal(err)
+			resp, err = ts.Client().Get(testUrl)
+		} else if e.method == "POST" {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
 			}
+			resp, err = ts.Client().PostForm(testUrl, values)
+		}
 
-			if resp.StatusCode != e.expectedStatusCode {
-				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
-			}
-		} else {
+		if err != nil {
+			t.Log(err)
+			t.Fatal(err)
+		}
 
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
 	}
 }
