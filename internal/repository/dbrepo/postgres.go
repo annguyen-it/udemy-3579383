44f2e993feb1ae2a0c2e773ffda943cece228fcd
annguyen <annguyen.it.utc@gmail.com>
2022-08-29 15:16:21 +0700

    13.105: Testing our insert reservation function

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -15,9 +15,9 @@ func (rp *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `INSERT INTO reservation 
+	stmt := `INSERT INTO reservations 
                 (first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at)
-             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
 	_, err := rp.DB.ExecContext(
 		ctx, stmt,
