020cb782c6ac91f261c5010a69f0fcc55e7cd134
annguyen <annguyen.it.utc@gmail.com>
2022-09-01 22:49:01 +0700

    13.110: Connecting search availability to the make reservation page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"learn-golang/internal/config"
 	"learn-golang/internal/driver"
 	"learn-golang/internal/forms"
@@ -241,4 +242,27 @@ func (rp *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request)
 			Data: data,
 		},
 	)
+}
+
+func (rp *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	rp.App.Session.Get(r.Context(), "reservation")
+
+	res, ok := rp.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res.RoomID = roomID
+
+	rp.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 }
