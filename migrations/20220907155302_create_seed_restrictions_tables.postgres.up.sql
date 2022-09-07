4691faafd1d4168271c336516f7cea9c71d8c307
annguyen <annguyen.it.utc@gmail.com>
2022-09-07 22:55:53 +0700

    14.118: Adding a migration for seeding restrictions

---
+++ migrations/20220907155302_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO restrictions (restriction_name, created_at, updated_at)
+VALUES ('Reservation', '2022-08-29 15:22:49.000000', '2022-08-29 15:22:49.000000'),
+       ('Owner Block', '2022-08-30 00:00:00.000000', '2022-08-30 00:00:00.000000');
